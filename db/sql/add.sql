c5c9ecda506eaced041ed45e06d9b4726e3bbab9
Nappy-Says <chokolatapapito@gmail.com>
2020-11-22 01:02:33 +0500

    some fix

--- db/sql/add.sql
+++ db/sql/add.sql
@@ -1,3 +1,7 @@
-INSERT INTO banners (id, title, content, button, link, image, created)
-VALUES (DEFAULT, 'Осуществите свои мечты', 'Вкладывайте депозит в Алифе и получайте больше половины от дохода',
-        'Вкладывать', 'https://alif.tj/deposit#CountYourIncome', 'granat.svg', DEFAULT);
+INSERT INTO banners (title, content, button, link, image) VALUES (
+		'Осуществите свои мечты',
+		'Вкладывайте депозит в Алифе и получайте больше половины от дохода',
+		'Вкладывать',
+		'https://alif.tj/deposit#CountYourIncome',
+		'granat.svg'
+	);
