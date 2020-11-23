5bb8d8e89c1b43cf57c642718687699213973dd8
Nappy-Says <chokolatapapito@gmail.com>
2020-11-23 18:38:23 +0500

    p

--- db/migration/000001_init_schema.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE banners (
-    id BIGSERIAL primary KEY,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
-    button TEXT NOT NULL,
-    link TEXT NOT NULL,
-    image TEXT NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
