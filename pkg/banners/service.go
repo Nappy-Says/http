ec9c7895d7f2bcc93de44b7b5ea1e5eaf368df09
Nappy <chokolatapapito@gmail.com>
2021-11-25 14:40:34 -0700

    types and not emplemented funcs

---
+++ pkg/banners/service.go
@@ -0,0 +1,50 @@
+package banners
+
+import (
+	"context"
+	"sync"
+)
+
+
+
+
+type Service struct {
+	mu 		sync.RWMutex
+	items 	[]*Banner
+}
+
+
+type Banner struct {
+	ID 		int64
+	Title 	string
+	Content string
+	Button 	string
+	Link	string
+}
+
+var panicNotEmplemented = "not emplemented"
+
+func NewService() *Service  {
+	return &Service{items: make([]*Banner, 0)}
+}
+
+
+
+func (s *Service) All(ctx context.Context) ([]*Banner, error) {
+	panic(panicNotEmplemented)	
+}
+
+
+func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
+	panic(panicNotEmplemented)	
+}
+
+
+func (s *Service) Save(ctx context.Context, item *Banner) ([]*Banner, error) {
+	panic(panicNotEmplemented)	
+}
+
+
+func (s *Service) RemoveByID(ctx context.Context, id int64) ([]*Banner, error) {
+	panic(panicNotEmplemented)	
+}
